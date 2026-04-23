69304070366ca825fb8e2890fe5dbee8a6900ea7
Guilherme Rodrigues <guilhermeaz.rodrigues@gmail.com>
2026-04-23 11:01:56 +0100

    Initial commit: Projeto Spring Boot para Laboratórios de Programação

---
+++ src/main/java/pt/ismt/clinicaVitae/ClinicaVitaeApplication.java
@@ -0,0 +1,13 @@
+package pt.ismt.clinicaVitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVitaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaVitaeApplication.class, args);
+	}
+
+}
